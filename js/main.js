10895b5e83932a517812bdc9dbcacdb41d5308e5
Yang <jiaoyangwfzc@gmail.com>
2017-08-15 17:27:15 +0800

    update new version.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 	function transformTime(){
 		var time = new Date();
-		var timeString = time.getFullYear()+"年 "+time.getMonth()+"月 "+time.getDate()+"日  星期"+transformDay(time.getDay())+"<br />"+time.getHours()+"点 "+time.getMinutes()+"分 "+time.getSeconds()+"秒 <br />"/*+time.getMilliseconds()*/;
+		var timeString = time.getFullYear()+"年 "+time.getMonth()+"月 "+time.getDate()+"日  星期"+transformDay(time.getDay())+"<br />"+time.getHours()+"点 "+time.getMinutes()+"分 "+time.getSeconds()+"秒 <br />";
 		return timeString;
 	}
 	function transformDay(day){
@@ -35,29 +35,28 @@
 		var title = document.getElementById("title"),
 			countTime = document.getElementById("countTime");
 		/*设置现在时间*/
-		var i = 0;
 		var t = setInterval(function(){
-					// console.log(transformTime())
-					document.getElementById('time').innerHTML = transformTime();
+            document.getElementById('time').innerHTML = transformTime();
 		},500);
-		if(!!window.localStorage){
-			console.log(localStorage.length);
-			if(!localStorage["titles"]&&!localStorage["dates"]&&!localStorage["times"]){
 
+		if(!!window.localStorage){
+			if(!localStorage["titles"]&&!localStorage["dates"]&&!localStorage["times"]){
 				var button = document.createElement("a");
 				button.appendChild(document.createTextNode("添加倒计时"));
 				document.getElementsByTagName("body")[0].appendChild(button);
-				console.log(button);
 				button.href = "setcounttime.html";
 				button.setAttribute("class","settime");
 
 			}else if(!!localStorage["dates"]&&!!localStorage["times"]){
-				title.innerHTML = localStorage["titles"]||'标题';
-				// countTime.innerHTML = localStorage["dates"]+" "+localStorage["times"]||'time';
+			    document.getElementById("time").style.display = "none";
+			    clearInterval(t);
+				title.innerHTML = (!!localStorage["titles"]?'离&nbsp'+localStorage["titles"]+'&nbsp还有：':'标题');
                 var date = localStorage["dates"].split("-"),
                     timeParse = date[0]+'/'+date[1]+'/'+date[2]+' '+localStorage["times"]+':00';
-                var endTime = Date.parse(timeParse);
+                var endTime = Date.parse(timeParse),
+                    text;
 
+                /*设置倒计时*/
 				var s = setInterval(function () {
 				    var nowTime = new Date();
 				    var times = endTime-nowTime.getTime();
@@ -65,17 +64,21 @@
                         day = Math.floor((times%(1000*60*60*24*365))/(1000*60*60*24)),
                         hour = Math.floor(((times%(1000*60*60*24*365))%(1000*60*60*24))/(1000*60*60)),
                         minutes = Math.floor((((times%(1000*60*60*24*365))%(1000*60*60*24))%(1000*60*60))/(1000*60)),
-                        secends = Math.floor(((((times%(1000*60*60*24*365))%(1000*60*60*24))%(1000*60*60))%(1000*60))/1000);
+                        seconds = Math.floor(((((times%(1000*60*60*24*365))%(1000*60*60*24))%(1000*60*60))%(1000*60))/1000);
 
-				    var texts = times>0?((year>=1?year+"年 ":"")+(day>=1?day+"天 ":"")+(hour>=1?hour+"小时 ":'')+(minutes>=1?minutes+"分 ":'')+(secends>=1?secends+'秒':'')):'时间到';
-                    countTime.innerHTML = texts;
+                    if (times<0) {
+                        clearInterval(s);
+                        text = "时间到";
+                    }else{
+                        text = (year>=1?year+"年 ":"")+(day>=1?day+"天 ":"")+(hour>=1?hour+"小时 ":'')+(minutes>=1?minutes+"分 ":'')+(seconds>=1?seconds+'秒':'')
+                    }
+                    countTime.innerHTML = text;
 
-                    if (times<0) clearInterval(s);
                 },500)
 			}
 		}
+
 		title.onclick = function(){
 			window.location.replace("setcounttime.html");
 		}
-
 	};
